3a2a42c3a0c6cf2d4ef3732db52e484ff0b7b7a0
DESKTOP-J13M1RV\82103 <rlaalstn1969@naver.com>
2022-05-17 17:00:14 +0900

    2022/05/17-feedback scheduling 2차마무리(주석/출력 정리)

--- Pythonpractice/scheduling_array.py
+++ Pythonpractice/scheduling_array.py
@@ -15,18 +15,19 @@ def readprocess_from_txt(): # txt파일 읽기
         intline=list(map(int,line))
         #print(intline)
         process.append(intline)
-    process = process[:-1]
+    process = process[:-1] # 끝에 0 자르기
     f.close()
-    global FIRST_PROCESS
-    FIRST_PROCESS = process
+    
+    global FIRST_PROCESS # 프로세스배열 초기상태
+    FIRST_PROCESS = process # 저장
     #print(process)
     return process
 # print(readprocess())
 
-NUM_OF_PROCESS = len(readprocess_from_txt())
-FEEDBACK_LEVEL = 5
-NUM_OF_PROCESS_ARG = 5
-NUM_OF_PRINT_ARGS = 6
+NUM_OF_PROCESS = len(readprocess_from_txt()) # 프로세스 개수
+FEEDBACK_LEVEL = 5                           # 피드백 레벨
+NUM_OF_PROCESS_ARG = 5                       # 프로세스 배열 인자 개수
+NUM_OF_PRINT_ARGS = 6                        # 출력할 배열의 인자개수
 # global return_time_list
 # return_time_list = list()
 
@@ -46,18 +47,19 @@ def run_process(p,seq,t): # 2차원 배열 입장 ex) [[1,0,3,0,1],[0,0,0,0,0],[
             pass
         else:
             service_time_list.append(p[i][2])
-    print("service_time_list\n",service_time_list)
+    # print("service_time_list\n",service_time_list)
     # service_time_list=list(filter(lambda x:x[4]))
     
     if p[seq][4] < FEEDBACK_LEVEL and not check_zeros(service_time_list): # 피드백 레벨이 5를 넘지 않았고 나머지의 서비스타임이 0배열이 아니라면
         p[seq][4] += 1
     return p
-
+##### sub definition #####
 ###########################################################################################################################################
 ###########################################################################################################################################
 ###########################################################################################################################################
 ###########################################################################################################################################
 ###########################################################################################################################################
+##### main definition #####
 
 def feedback_sc(process):
     process.sort(key=lambda x:x[1]) # 도착시간 빠른순으로 정렬
@@ -78,7 +80,7 @@ def feedback_sc(process):
     # print("arriv time list\n",arriv_time_list)
     
     global end_time_list
-    end_time_list = list()
+    end_time_list = list() # 종료시간 배열
     
     # print(process) [[1, 0, 3, 0, 1], [2, 2, 6, 0, 1], [3, 4, 4, 0, 1], [4, 6, 5, 0, 1], [5, 8, 2, 0, 1]]
     # table = np.zeros(total_service_time * NUMBER_OF_ARG * LEN_OF_PROCESS).reshape(total_service_time,NUMBER_OF_ARG,LEN_OF_PROCESS)
@@ -89,11 +91,15 @@ def feedback_sc(process):
     state_of_process = -1 # 직전에 실행한 프로세스의 위치(초기값 = -1)
     # table 세팅 및 처리
     for i in range(total_service_time + 1): # i: 현재시간
-        print("time: ",i) # i = 0일땐 추가만 하고 끝
+        ########## 출력테스트 ##########
+        # print("time: ",i) # i = 0일땐 추가만 하고 끝
+        ########## 출력테스트 ##########
         if (i == 0):
             table[0] = process[0]
-            print("table\n",table)
-            print()
+            ########## 출력테스트 ##########
+            # print("table\n",table) # [[1,0,3,0,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+            # print()
+            ########## 출력테스트 ##########
             continue
         
         for j in range(NUM_OF_PROCESS):
@@ -114,19 +120,21 @@ def feedback_sc(process):
         if (state_of_process != -1):
             temp = processing_list[state_of_process]
         # 피드백 레벨순으로 정렬된 배열
-        processing_list = np.array(sorted(processing_list,key = lambda x:x[0] if (x[4] != 0) else 4))
-        processing_list = np.array(sorted(processing_list,key = lambda x:x[4] if (x[4] != 0) else 4))
+        processing_list = np.array(sorted(processing_list,key = lambda x:x[0] if (x[4] != 0) else 4)) # id순 정렬
+        processing_list = np.array(sorted(processing_list,key = lambda x:x[4] if (x[4] != 0) else 4)) # 피드백레벨순 정렬
         
         if (state_of_process != -1): # state_of_process가 설정된 후 배열이 정렬되기 때문에 state_of_process 재설정
             for j in range(NUM_OF_PROCESS):
                 if np.array_equal(processing_list[j], temp):
                     state_of_process = j
                     break
-        # print("processing list: ",processing_list) # ex) [[2.2.6.0.1],[1,0,1,0,3],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+        # print("processing list\n",processing_list) # ex) [[2.2.6.0.1],[1,0,1,0,3],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]]
+        
+        ########## 출력테스트 ##########
+        # print("before\n",processing_list)
+        ########## 출력테스트 ##########
         
         # 작업 수행 - 0을 제외한 피드백 레벨순으로 정렬된 배열이므로 맨 앞의 것을 수행한다
-        print("before\n",processing_list)
-        
         for j in range(NUM_OF_PROCESS):
             # 남은 q가 0보다 크고 지금 막 들어온 프로세스가 아니면
             if (processing_list[j][2] > 0) and (processing_list[j][1] != i):
@@ -136,13 +144,16 @@ def feedback_sc(process):
                 ##### 해결 #####
                 # 직전에 실행한 프로세스면
                 if (state_of_process == j) and (processing_list[state_of_process][4] == FEEDBACK_LEVEL):
-                    # 건너뛰기
-                    continue
+                    continue # 건너뛰기
                 # 직전에 실행한 프로세스가 아니면
-                processing_list = run_process(processing_list,j,i)
-                print("now processing: ",processing_list[j]) # 실행한 리스트 출력
+                processing_list = run_process(processing_list,j,i) # 프로세싱할 프로세스배열, 현재 위치(p1~p5), 현재 시간
+                
+                ########## 출력테스트 ##########
+                # print("now processing: ",processing_list[j]) # 실행한 리스트 출력
+                ########## 출력테스트 ##########
+                
                 state_of_process = j # 실행한 프로세스의 위치 저장
-                break
+                break # 프로세스 하나 실행했으면 반복문 탈출
             
         # print("지금 막 들어온 프로세스의 도착시간\n",processing_list[state_of_process][1])
         for j in range(NUM_OF_PROCESS): # 실행한 프로세스 제외한 나머지 프로세스의 대기시간 증가
@@ -151,18 +162,32 @@ def feedback_sc(process):
             else:
                 if (processing_list[j][2] > 0) and (i != processing_list[j][1]): # 단, 남은 q가 있고 방금 도착한 프로세스가 아닌 경우만
                     processing_list[j][3] += 1
-        print("after\n",processing_list)
-        print()
+        
+        ########## 출력테스트 ##########
+        # print("after\n",processing_list)
+        # print()
+        ########## 출력테스트 ##########
         
 
         for j in range(NUM_OF_PROCESS):
             if processing_list[j][0] != 0:
                 table[j] = processing_list[j] # 모두 수행한 리스트를 테이블로 반환
-
+    ##### △ 여기까지 반복문 △ #####
+    ##### 프로세스 수행 완료 #####
+    ###########################################################################################################################################
+    ###########################################################################################################################################
+    ###########################################################################################################################################
+    ###########################################################################################################################################
+    ###########################################################################################################################################
+    ##### 출력 단계 #####
     last_table = np.array(sorted(table, key = lambda x:x[0])) # 최종테이블 id기준 정렬
     end_time_list = np.array(sorted(end_time_list, key = lambda x:x[0])) # 반환시간 리스트 id기준 정렬
-    print("lasttable\n",last_table)
+    
+    ########## 출력테스트 ##########
+    # print("lasttable\n",last_table)
     # 최종 테이블: [[ 1.  0.  0.  1.  3.],[ 2.  2.  0. 12.  5.], [ 3.  4.  0.  8.  5.], [ 4.  6.  0.  8.  5.], [ 5.  8.  0.  1.  3.]]
+    ########## 출력테스트 ##########
+    
     # print("arriv time list\n",arriv_time_list)
     # 도착시간 리스트: [0, 2, 4, 6, 8]
     # print("end_time_list\n",end_time_list)
@@ -197,10 +222,15 @@ def feedback_sc(process):
     
 
 result = feedback_sc(readprocess_from_txt())
-result = np.array(sorted(result,key = lambda x:x[3])) # 도착순 정렬
+result = np.array(sorted(result,key = lambda x:x[3])) # 종료순 정렬
 
-for i in range(NUM_OF_PROCESS):
+########## 출력테스트 ##########
+# print("result\n",result)
+########## 출력테스트 ##########
+
+for i in range(NUM_OF_PROCESS): # 최종결과 반환
+    print("process ( id =",int(result[i][0]),") 종료")
     print("[id / 도착 / 서비스 / 종료 / 반환 / 정규화반환]\n",result[i])
-    print("")
-# print("result\n","[id / 도착 / 서비스 / 종료 / 반환 / 정규화반환]\n",feedback_sc(readprocess_from_txt()))
-# print("FIRST PROCESS\n",FIRST_PROCESS)
+    print()
+
+# 별생각없이 대기시간도 구했는데 쓸데가 없네요
